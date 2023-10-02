4ab94f86f020770283d522cbe4eebe04cee1ce95
Yevhenii Zh <yevheniizhyrov.dev@gmail.com>
2023-10-02 23:59:26 +0300

    Space macaroni with instances

--- src/Particles.js
+++ src/Particles.js
@@ -1,21 +1,62 @@
 
-import { useRef } from 'react';
+import { useRef, useMemo, useEffect } from 'react';
 import * as THREE from 'three';
+import { MeshPhysicalMaterial, MeshMatcapMaterial } from 'three';
 import './RenderMaterial'
 import './SimulationMaterial'
 import { getDataTexture, getSphereTexture, getVelocityTexture } from './getDataTexture';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import { GPUComputationRenderer } from 'three/examples/jsm/misc/GPUComputationRenderer';
+
+import CustomShaderMaterial from 'three-custom-shader-material'
+import { patchShaders } from 'gl-noise/build/glNoise.m'
 
 import simulationFragmentPosition from './shaders/simulationFragmentPosition';
 import simulationFragmentVelocity from './shaders/simulationFragmentVelocity';
 
-export function Particles(){
-    const SIZE = 16;
-    const { gl, viewport} = useThree();
+const shader = {
+    vertex: /* glsl */ `
+        uniform float uTime;
+        uniform sampler2D uPosition;
+        uniform sampler2D uVelocity;
+        attribute vec2 ref;
 
-    const simulationMaterial = useRef();
-    const renderMaterial = useRef();
+        vec3 rotate3D(vec3 v, vec3 vel) {
+            vec3 newpos = v;
+            vec3 up = vec3(0, 1, 0);
+            vec3 axis = normalize(cross(up, vel));
+            float angle = acos(dot(up, normalize(vel)));
+            newpos = newpos * cos(angle) + cross(axis, newpos) * sin(angle) + axis * dot(axis, newpos) * (1. - cos(angle));
+            return newpos;
+        }
+  
+        vec3 displace(vec3 point, vec3 vel) {
+            vec3 pos = texture2D(uPosition,ref).rgb;
+            vec3 copypoint = rotate3D(point, vel);
+            vec3 instancePosition = (instanceMatrix * vec4(copypoint, 1.)).xyz;
+            return instancePosition + pos;
+        }
+
+        void main() {
+            vec3 vel = texture2D(uVelocity,ref).rgb;
+            vec3 p = displace(position, vel);
+            csm_PositionRaw = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(p, 1.);
+            csm_Normal = rotate3D(normal, vel);
+          }
+      `,
+    fragment: /* glsl */ `
+      void main() {
+        csm_DiffuseColor = vec4(1.);
+      }
+      `,
+  }
+
+export function Particles(){
+    const SIZE = 256;
+    const { gl, viewport} = useThree();
+    const [map] = useLoader(THREE.TextureLoader, ['./matcap4.png']);
+
+    const iref = useRef();
     const followMouse = useRef();
 
     const gpuCompute = new GPUComputationRenderer( SIZE, SIZE, gl );
@@ -56,34 +97,39 @@ export function Particles(){
         }
     }
 
-    const originalPosition = getDataTexture(SIZE);
+    const uniforms = useMemo( () => ({
+        uPosition: {
+            value: null,
+        },
+        uVelocity: {
+            value: null,
+        },
+    }), [] );
+
+    useEffect(() => {
+        const ref = new Float32Array(SIZE * SIZE * 2);
+        for (let i = 0; i < SIZE; i++) {
+            for (let j = 0; j < SIZE; j++) {
+                const k = i * SIZE + j;
+                ref[k * 2 + 0] = i / (SIZE-1);
+                ref[k * 2 + 1] = j / (SIZE-1);
+            }
+        }
+
+        iref.current.geometry.setAttribute('ref', new THREE.InstancedBufferAttribute(ref, 2))    }, []);
 
     useFrame(({mouse}) => {
-        followMouse.current.position.x = mouse.x * viewport.width / 2;
-        followMouse.current.position.y = mouse.y * viewport.height / 2;
-
-        velocityUniforms.uMouse.value.x = followMouse.current.position.x;
-        velocityUniforms.uMouse.value.y = followMouse.current.position.y;
+        followMouse.current.position.x = (mouse.x * viewport.width) / 2;
+        followMouse.current.position.y = (mouse.y * viewport.height) / 2;
+    
+        velocityUniforms.uMouse.value.x = (mouse.x * viewport.width) / 2;
+        velocityUniforms.uMouse.value.y = (mouse.y * viewport.height) / 2;
     });
 
     useFrame(({gl}) => {
         gpuCompute.compute();
-        renderMaterial.current.uniforms.uPosition.value = gpuCompute.getCurrentRenderTarget(positionVariable).texture;
-
-
-
-        // gl.setRenderTarget(target0);
-        // gl.render(scene, camera);
-        // gl.setRenderTarget(null);
-
-        // renderMaterial.current.uniforms.uPosition.value = target1.texture;
-        // simulationMaterial.current.uniforms.uPosition.value = target0.texture;
-
-        // // [target0, target1] = [target1, target0];
-
-        // let temp = target0;
-        // target0 = target1;
-        // target1 = temp;
+        iref.current.material.uniforms.uPosition.value = gpuCompute.getCurrentRenderTarget(positionVariable).texture;
+        iref.current.material.uniforms.uVelocity.value = gpuCompute.getCurrentRenderTarget(velocityVariable).texture;
     });
 
     return(
@@ -92,23 +138,19 @@ export function Particles(){
                 <sphereGeometry args={[0.1, 32, 32]} />
                 <meshBasicMaterial color="red" />
             </mesh>
-            <points>
-                <bufferGeometry>
-                    <bufferAttribute
-                        attach="attributes-position"
-                        count ={particles.length / 3}
-                        array={particles}
-                        itemSize={3}
-                    />
-                    <bufferAttribute
-                        attach="attributes-ref"
-                        count ={ref.length / 3}
-                        array={ref}
-                        itemSize={2}
-                    />
-                </bufferGeometry>
-                <renderMaterial transparent blending={THREE.AdditiveBlending} ref={renderMaterial} />
-            </points>
+
+            <instancedMesh ref={iref} args={[null, null, SIZE*SIZE]}>
+                <boxGeometry args={[0.01, 0.03, 0.01]} />
+                <CustomShaderMaterial
+                    baseMaterial={MeshMatcapMaterial}
+                    size={0.01}
+                    matcap={map}
+                    vertexShader={patchShaders(shader.vertex)}
+                    fragmentShader={patchShaders(shader.fragment)}
+                    uniforms={uniforms}
+                    transparent
+                />
+            </instancedMesh>
         </>
     )
 }
